e998b7e4d44e73df23e3434615aab89e32f7874b
AYehia0 <ahmedyehia611@gmail.com>
2021-12-02 02:13:52 +0200

    script to backup .config

---
+++ install.sh
@@ -0,0 +1,32 @@
+: ' 
+A simple script that backups the old dotfiles and installs the newer one : Bspwmonly.
+
+The setup requirements :
+
+    - bspwm
+    - sxhkd
+    - polybar
+    - picom
+    - kitty
+    - urxvt
+    - w3m
+    - ranger
+    - nvim
+    - rofi
+    - dunst
+    - flameshot
+    - qutebrowser
+'
+
+# backup dots in .config
+zip -rq dotsfiles_back \
+    ~/.config/bspwm \
+    ~/.config/sxhkd \
+    ~/.config/polybar \
+    ~/.config/picom \
+    ~/.config/nvim \
+    ~/.config/ranger \
+    ~/.config/dunst \
+    ~/.config/kitty \
+    ~/.config/qutebrowser \
+
